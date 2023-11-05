cb63b7538adb73b0931bf5ede78c377e5df9ddd6
Cleilton Rocha <cleiltonrv30@gmail.com>
2023-11-05 12:54:27 -0300

    Status Component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,25 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
-
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
           <label htmlFor="minutesAmount">Durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos</span>
@@ -36,7 +48,7 @@ export function Home() {
         </CountDownContainer>
 
         <StartCountDownButton type="submit">
-          <Play /> Começar
+          <Play size={24} /> Começar
         </StartCountDownButton>
       </form>
     </HomeContainer>
